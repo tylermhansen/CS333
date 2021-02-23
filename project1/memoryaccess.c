802d072d87e9f0c87890249f741e6b92cb21df20
Tyler Hansen <TyHansen00@gmail.com>
2021-02-23 18:39:11 -0500

    Remove extraneous generic file and apply styling to `memoryaccess.c`

--- project1/memoryaccess.c
+++ project1/memoryaccess.c
@@ -14,51 +14,53 @@ C's sizeof() method to explore how many bytes are allocated in memory for each t
 and the endian-ness of my machine.
 */ 
 int main (int arg, char *argv[]) {
+  
   char var_char = 'c';
   short var_short = 32767;
   int var_int = 0x01234567;
   long var_long = 4242;
   float var_float = 4242.42;
   double var_double = 4242.4242;
-  
+    
   int i = 0x01234567;
   unsigned char *ptr;
 
   ptr = (unsigned char *)&(var_int);
   printf("Integer Loop:\n");
   for(int i=0; i<sizeof(int); i++) {
-	    printf("%d: %02X\n", i, ptr[i]);
-	}
+    printf("%d: %02X\n", i, ptr[i]);
+  }
 
   ptr = (unsigned char *)&(var_double);
   printf("Double Loop:\n");
   for(int i=0; i<sizeof(double); i++) {
-	    printf("%d: %02X\n", i, ptr[i]);
-	}
+    printf("%d: %02X\n", i, ptr[i]);
+  }
 
   ptr = (unsigned char *)&(var_short);
   printf("Short Loop:\n");
   for(i=0; i<sizeof(short); i++) {
-	    printf("%d: %02X\n", i, ptr[i]);
-	}
+    printf("%d: %02X\n", i, ptr[i]);
+  }
 
   ptr = (unsigned char *)&(var_long);
   printf("Long Loop:\n");
   for(i=0; i<sizeof(long); i++) {
-	    printf("%d: %02X\n", i, ptr[i]);
-	}
+    printf("%d: %02X\n", i, ptr[i]);
+  }
 
   ptr = (unsigned char *)&(var_float);
   printf("Float Loop:\n");
   for(i=0; i<sizeof(float); i++) {
-	    printf("%d: %02X\n", i, ptr[i]);
-	}
+    printf("%d: %02X\n", i, ptr[i]);
+  }
 
   ptr = (unsigned char *)&(var_char);
   printf("Character Loop:\n");
   for(i=0; i<sizeof(char); i++) {
-	    printf("%d: %02X\n", i, ptr[i]);
-	}
+    printf("%d: %02X\n", i, ptr[i]);
+  }	
 				
-      return 0;
-    }  
+  return 0;
+
+}  
